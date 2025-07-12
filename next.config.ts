f3e1b86fcf265b9c084d3eec25eb6f3751efb328
Wayne Casten (Mayne Inspectors) <castenhome@gmail.com>
2025-07-12 06:34:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestudio.corp.google.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
